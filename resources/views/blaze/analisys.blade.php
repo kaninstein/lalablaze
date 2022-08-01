15cd41dd98984926b224ac7d3778f05c96807272
projetandoasvendas <201803396946@alunos.estacio.br>
2022-07-31 22:37:15 -0300

    analisys2

--- resources/views/blaze/analisys.blade.php
+++ resources/views/blaze/analisys.blade.php
@@ -141,6 +141,7 @@
                     + {{ ($sign->number + $sign->getColor()->number) }}
                     @elseif($sign->count_mode == 3)
                     + {{ ($sign->number * $sign->getColor()->number) }}
+                    @endif
                 </td>
                 <td class="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400 text-center">
                     <div class="flex flex-row justify-center space-x-8">
